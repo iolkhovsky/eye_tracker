b98f99f0506263c11c3036decc54096edd638e16
Igor <i.v.olkhovsky@gmail.com>
2021-08-21 21:58:33 +0300

    Fix bug in tfrecord generation tool

--- dataset/generate_tfrecord.py
+++ dataset/generate_tfrecord.py
@@ -5,54 +5,32 @@ from random import shuffle
 import tensorflow as tf
 from tqdm import tqdm
 
+from dataset.tfrecord_utils import *
 from utils.file_utils import read_yaml
-
-
-def _int64_feature(value):
-    """Returns an int64_list from a bool / enum / int / uint."""
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _float_feature(value):
-    """Returns a float_list from a float / double."""
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
-
-
-def _float_feature_list(value):
-    """Returns a float_list from a float / double."""
-    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
-
-
-def _bytes_feature(value):
-    """Returns a bytes_list from a string / byte."""
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def create_tf_example(annotation):
     img_path = annotation["source_image_path"]
-    with tf.gfile.GFile(img_path, 'rb') as fid:
+    with tf.io.gfile.GFile(img_path, 'rb') as fid:
         encoded_jpg = fid.read()
-    img_width = annotation["image_width"]
-    img_height = annotation["image_height"]
-    img_channels = annotation["image_channels"]
     feature = {
-        'image/height': _int64_feature(img_height),
-        'image/width': _int64_feature(img_width),
-        'image/depth': _int64_feature(img_channels),
-        'image/filename': _bytes_feature(img_path.encode('utf8')),
-        'image/encoded': _bytes_feature(encoded_jpg),
-        "equation/a": _float_feature(annotation["markup"]["equation"]["a"]),
-        "equation/b": _float_feature(annotation["markup"]["equation"]["b"]),
-        "equation/c": _float_feature(annotation["markup"]["equation"]["c"]),
-        "equation/d": _float_feature(annotation["markup"]["equation"]["e"]),
-        "equation/e": _float_feature(annotation["markup"]["equation"]["d"]),
-        "equation/f": _float_feature(annotation["markup"]["equation"]["f"]),
-        "canonical/a": _float_feature(annotation["markup"]["canonical"]["a"]),
-        "canonical/b": _float_feature(annotation["markup"]["canonical"]["b"]),
-        "canonical/x": _float_feature(annotation["markup"]["canonical"]["x"]),
-        "canonical/y": _float_feature(annotation["markup"]["canonical"]["y"]),
-        "canonical/t": _float_feature(annotation["markup"]["canonical"]["t"]),
-
+        'image/height': int64_feature(annotation["image_height"]),
+        'image/width': int64_feature(annotation["image_width"]),
+        'image/depth': int64_feature(annotation["image_channels"]),
+        'image/filename': bytes_feature(img_path.encode('utf8')),
+        'image/encoded': bytes_feature(encoded_jpg),
+        "pupil": int64_feature(annotation["markup"]["pupil"]),
+        "equation/a": float_feature(annotation["markup"]["equation"]["a"]),
+        "equation/b": float_feature(annotation["markup"]["equation"]["b"]),
+        "equation/c": float_feature(annotation["markup"]["equation"]["c"]),
+        "equation/d": float_feature(annotation["markup"]["equation"]["e"]),
+        "equation/e": float_feature(annotation["markup"]["equation"]["d"]),
+        "equation/f": float_feature(annotation["markup"]["equation"]["f"]),
+        "canonical/a": float_feature(annotation["markup"]["canonical"]["a"]),
+        "canonical/b": float_feature(annotation["markup"]["canonical"]["b"]),
+        "canonical/x": float_feature(annotation["markup"]["canonical"]["x"]),
+        "canonical/y": float_feature(annotation["markup"]["canonical"]["y"]),
+        "canonical/t": float_feature(annotation["markup"]["canonical"]["teta"]),
     }
     tf_example = tf.train.Example(features=tf.train.Features(feature=feature))
     return tf_example
@@ -78,24 +56,26 @@ def run_tfrecord_generation(args):
         test_dataset_path = splitext(train_dataset_path)[0] + "_test.tfrecord"
     if args.val_share + args.test_share > 0:
         train_dataset_path = splitext(train_dataset_path)[0] + "_train.tfrecord"
-    train_writer = tf.python_io.TFRecordWriter(train_dataset_path)
-    val_writer = tf.python_io.TFRecordWriter(val_dataset_path) if val_dataset_path is not None else None
-    test_writer = tf.python_io.TFRecordWriter(test_dataset_path) if test_dataset_path is not None else None
+    train_writer = tf.io.TFRecordWriter(train_dataset_path)
+    val_writer = tf.io.TFRecordWriter(val_dataset_path) if val_dataset_path is not None else None
+    test_writer = tf.io.TFRecordWriter(test_dataset_path) if test_dataset_path is not None else None
 
     imgs_list = glob(join(args.dataset, "*.jpg"))
     annotations_list = glob(join(args.dataset, "*.yml"))
-    samples_list = set([splitext(x)[0] for x in imgs_list]).intersection(
+    samples_list = list(
+        set([splitext(x)[0] for x in imgs_list]).intersection(
         set([splitext(x)[0] for x in annotations_list]))
+    )
     total_samples = len(samples_list)
     print(f"Found {total_samples} samples at: {args.dataset}")
     shuffle(samples_list)
-    val_samples_cnt = int(args.val_share) * total_samples if args.val_share is not None else 0
-    test_samples_cnt = int(args.test_share) * total_samples if args.val_share is not None else 0
+    val_samples_cnt = int(args.val_share * total_samples) if args.val_share is not None else 0
+    test_samples_cnt = int(args.test_share * total_samples) if args.test_share is not None else 0
     train_samples_cnt = total_samples - val_samples_cnt - test_samples_cnt
 
     train_samples = samples_list[:train_samples_cnt]
-    val_samples = samples_list[train_samples_cnt:train_samples + val_samples_cnt]
-    test_samples = samples_list[train_samples + val_samples_cnt:]
+    val_samples = samples_list[train_samples_cnt:train_samples_cnt + val_samples_cnt]
+    test_samples = samples_list[train_samples_cnt + val_samples_cnt:]
 
     for samples, writer, hint in [(train_samples, train_writer, "train"),
                                   (val_samples, val_writer, "val"),
---
+++ dataset/tfrecord_utils.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def float_feature_list(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))

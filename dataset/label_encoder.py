4cf5e602abb95d19262de84325b41cc5d2f4b80c
Igor <i.v.olkhovsky@gmail.com>
2021-08-26 15:10:18 +0300

    Refactor label encoder

--- dataset/label_encoder.py
+++ dataset/label_encoder.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tensorflow as tf
 
 from dataset.tfrecord_utils import decode_fn
@@ -16,17 +15,16 @@ class LabelEncoder:
         batch = decode_fn(batch)
         img = tf.io.decode_jpeg(batch["image/encoded"])
         img = tf.cast(img, dtype=tf.float32)
-        img_size = tf.cast(batch["image/width"], tf.float32)
         if self.normalizer is not None:
             img = self.normalizer(img)
         label = tf.stack(
             [
                 tf.cast(batch["pupil"], dtype=tf.float32),
-                batch["canonical/a"] / img_size,
-                batch["canonical/b"] / img_size,
-                batch["canonical/x"] / img_size,
-                batch["canonical/y"] / img_size,
-                batch["canonical/t"] / tf.constant(0.5 * np.pi, dtype=tf.float32)
+                batch["canonical/a"],
+                batch["canonical/b"],
+                batch["canonical/x"],
+                batch["canonical/y"],
+                batch["canonical/t"]
             ],
             axis=0
         )
